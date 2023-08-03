27b267ec388c4fd47b1801ce730c1191a21c7e70
ayemin6796 <diamalt541@gmail.com>
2023-08-03 22:55:27 +0630

    auth

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        User::factory()->create([
+            "name" => "Aung Hmue",
+            "email" => "aht@gmail.com",
+            "password" => Hash::make("asdffdsa"),
+        ]);
+
+        User::factory(4)->create();
+
+        User::factory()->create([
+            "name" => "Aye Min Tun",
+            "email" => "amt@gmail.com",
+            "password" => Hash::make("asdffdsa"),
+            "role" => "admin"
+        ]);
+    }
+}
