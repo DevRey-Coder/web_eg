ee50df79f23cd4fc997729e27b3bfc884454fb7e
AungHmueThet <seriousglobin@gmail.com>
2023-08-04 00:54:40 +0630

    mig

--- database/migrations/2023_08_03_161434_create_brands_table.php
+++ database/migrations/2023_08_03_161434_create_brands_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('brands', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role',["admin","user"])->default("user");
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->char("name");
+            $table->char("company");
+            $table->text("information");
+            $table->foreignId("user_id");
+            $table->char("photo");
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('brands');
     }
 };
--- database/migrations/2023_08_03_161544_create_vouchers_table.php
+++ database/migrations/2023_08_03_161544_create_vouchers_table.php
@@ -11,14 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('vouchers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role',["admin","user"])->default("user");
-            $table->timestamp('email_verified_at')->nullable();
-            $table->rememberToken();
+            $table->char("customer");
+            $table->char("phone")->nullable();
+            $table->char("voucher");
+            $table->bigInteger("total");
+            $table->bigInteger("tax");
+            $table->bigInteger("net_total");
+            $table->foreignId("user_id");
             $table->timestamps();
         });
     }
@@ -28,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('vouchers');
     }
 };
